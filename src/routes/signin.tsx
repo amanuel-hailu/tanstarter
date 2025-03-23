7536d4945ad10ab2c0d573a5bb0d466fa64f0538
amanuel-hailu <amanuel.hailu@email.sitel.org>
2025-03-23 13:35:43 -0400

    comment out unused social providers in auth configuration and disable Discord and GitHub sign-in buttons in the AuthPage

--- src/routes/signin.tsx
+++ src/routes/signin.tsx
@@ -23,7 +23,7 @@ function AuthPage() {
       <div className="flex flex-col items-center gap-8 rounded-xl border bg-card p-10">
         Logo here
         <div className="flex flex-col gap-2">
-          <SignInButton
+          {/* <SignInButton
             provider="discord"
             label="Discord"
             className="bg-[#5865F2] hover:bg-[#5865F2]/80"
@@ -32,7 +32,7 @@ function AuthPage() {
             provider="github"
             label="GitHub"
             className="bg-neutral-700 hover:bg-neutral-700/80"
-          />
+          /> */}
           <SignInButton
             provider="google"
             label="Google"
